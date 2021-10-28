e5c201732ac853e1fd195ee7616e637fabde5c3f
Curtis Tarr <ctarr901@gmail.com>
2021-10-28 12:32:12 +0100

    Adding commented wsl_setup.sh (non-working)

---
+++ wsl_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# sets up my dev environment in WSL Ubuntu without sudo
+
+# setup global get config
+# git config --global user.name "Your Name"
+# git config --global user.email your@email.com
+
+# setup rust
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# setup node + yarn
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+# nvm install 16
+# npm install --global yarn
+
+# setup vue cli
+# yarn global add @vue/cli
